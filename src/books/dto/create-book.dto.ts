891d26d2882b1c54e9267c4c2f075dc7c6b9013a
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-11-26 03:43:55 -0600

    Add: dto de book y entiti

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,33 @@
-export class CreateBookDto {}
+import { Type } from "class-transformer";
+import { IsNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateBookDto {
+
+   @IsString()
+   public isbn: string
+
+   @IsString()
+   public titulo: string
+
+   @IsString()
+   public tipo: string
+
+   @IsString()
+   public autor: string
+
+   @IsNumber()
+   @IsPositive()
+   @Type(() => Number)
+   public status: number
+
+   @IsNumber()
+   @IsPositive()
+   @Type(() => Number)
+   public genero: number
+
+   @IsNumber()
+   @IsPositive()
+   @Type(() => Number)
+   public categoria: number
+
+}
--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,11 @@
-export class Book {}
+export class Book {
+
+   public isbn: string
+   public titulo: string
+   public tipo: string
+   public autor: string
+   public status: number
+   public genero: number
+   public categoria: number
+
+}
