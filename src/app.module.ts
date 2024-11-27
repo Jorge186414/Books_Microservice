79fedd6338825749e64e18f6292c8c0c15dbebae
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-11-27 09:10:52 -0600

    Add: Importaciones de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
+import { CategoryModule } from './category/category.module';
+import { ReservesModule } from './reserves/reserves.module';
+import { GenresModule } from './genres/genres.module';
+import { LoansModule } from './loans/loans.module';
+import { LoandetailsModule } from './loandetails/loandetails.module';
+import { ReturnloansModule } from './returnloans/returnloans.module';
+import { ReturnloandetailsModule } from './returnloandetails/returnloandetails.module';
+import { PenaltiesModule } from './penalties/penalties.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, CategoryModule, ReservesModule, GenresModule, LoansModule, LoandetailsModule, ReturnloansModule, ReturnloandetailsModule, PenaltiesModule],
   controllers: [],
   providers: [],
 })
